dadf16129bf7c98818c7da04cceadfcd81d0a2cd
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2017-12-18 14:46:44 +0200

    Add more resources and providers to terraform

--- droplet.tf
+++ droplet.tf
@@ -2,6 +2,7 @@ variable "do_token" {}
 variable "pub_key" {}
 variable "pvt_key" {}
 variable "ssh_fingerprint" {}
+variable "user" {}
 
 # Set digitalocean as provider
 provider "digitalocean" {
@@ -18,16 +19,50 @@ resource "digitalocean_droplet" "nclandrei" {
 	ssh_keys = [
 		"${var.ssh_fingerprint}"
 	]
+
+	provisioner "remote-exec" {
+		inline = [
+			"echo installing core packages",
+			"rm /etc/motd # it does not spark joy",
+			"echo 'deb http://download.opensuse.org/repositories/shells:/fish:/release:/2/Debian_9.0/ /' > /etc/apt/sources.list.d/fish.list",
+			"wget -nv https://download.opensuse.org/repositories/shells:fish:release:2/Debian_9.0/Release.key -O Release.key >>/root/provisioning.log 2>&1",
+			"apt-key add - < Release.key >>/root/provisioning.log 2>&1",
+			"apt-get update >>/root/provisioning.log 2>&1",
+			"apt-get install -y --force-yes sudo make vim git mercurial mosh fish curl wget unzip htop jq binutils gcc libpcap-dev >>/root/provisioning.log 2>&1",
+
+			"echo installing Go",
+			"wget https://storage.googleapis.com/golang/go1.9.2.linux-amd64.tar.gz >>/root/provisioning.log 2>&1",
+			"tar -C /usr/local -xzf go1.9.2.linux-amd64.tar.gz >>/root/provisioning.log 2>&1",
+			"rm go1.9.2.linux-amd64.tar.gz",
+			
+			"echo setting up user ${var.user}",
+			"sed -i.bak 's/sudo\tALL=(ALL:ALL) ALL/sudo\tALL=(ALL:ALL) NOPASSWD: ALL/g' /etc/sudoers",
+			"adduser --shell /usr/bin/fish --ingroup sudo --disabled-password --gecos '' ${var.user} >>/root/provisioning.log 2>&1",
+			"mkdir -p /home/${var.user}/.ssh",
+			"chown -R ${var.user}:sudo /home/${var.user}/.ssh",
+		]
+	}
+
+	connection {
+    	user = "root"
+    	type = "ssh"
+    	private_key = "${file(var.pvt_key)}"
+    	timeout = "2m"
+  }
 }
 
+# Configure domain name on droplet
 resource "digitalocean_domain" "default" {
 	name = "nclandrei.com"
-	ip = {}
+	ip = "${digitalocean_droplet.nclandrei.ipv4_address}"
 }
 
-connection {
-      user = "root"
-      type = "ssh"
-      private_key = "${file(var.pvt_key)}"
-      timeout = "2m"
-  }
+# Redirect www.nclandrei.com to nclandrei.com
+resource "digitalocean_record" "CNAME-www" {
+  domain = "${digitalocean_domain.default.name}"
+  type = "CNAME"
+  name = "www"
+  value = "@"
+}
+
+
