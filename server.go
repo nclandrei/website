f30fa3bcf913a500c0c7bd06a43d435d8684c1a6
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2017-12-20 19:34:15 +0200

    Test blog handler

--- server.go
+++ server.go
@@ -5,12 +5,22 @@ import (
 	"net/http"
 )
 
+// IndexTemplate defines variables used on the index page
+type IndexTemplate struct {
+	Title string
+}
+
 // Basic handler
 func viewHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>HELLO WORLD!</h1>")
 }
 
+func blogHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "templates/index.html")
+}
+
 func main() {
 	http.HandleFunc("/", viewHandler)
+	http.HandleFunc("/blog", blogHandler)
 	http.ListenAndServe(":8080", nil)
 }
